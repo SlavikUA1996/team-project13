85b7328bc1e0ebaf1704a2e21575d72d0ff3d000
Myroslava-Benca <mashaivanova685@gmail.com>
2023-01-17 04:41:55 +0200

    new change

--- js/modal-product.js
+++ js/modal-product.js
@@ -12,18 +12,3 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-menu-product-open]"),
-    closeModalBtn: document.querySelector("[data-menu-product-close]"),
-    modal: document.querySelector("[data-menu-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
