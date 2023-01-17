256473d9e8009174ab68ddbaf134d2ba439bb89e
SlavikUA1996 <115225325+SlavikUA1996@users.noreply.github.com>
2023-01-17 03:06:03 +0000

    Deploying to gh-pages from @ SlavikUA1996/team-project13@02e328453a1e6a0a7314e82005af75632ca97f54 🚀

---
+++ index.d37ccb32.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-product-open]"),closeModalBtn:document.querySelector("[data-modal-product-close]"),modal:document.querySelector("[data-product-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.d37ccb32.js.map
