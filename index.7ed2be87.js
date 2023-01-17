6e8fcf8fce106ee2fa75027cd3c98a2f65a50f1a
SlavikUA1996 <115225325+SlavikUA1996@users.noreply.github.com>
2023-01-17 02:44:35 +0000

    Deploying to gh-pages from @ SlavikUA1996/team-project13@1fdb45f5f8dd18f6e6b69eabea447365ad9f499a 🚀

---
+++ index.7ed2be87.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-7]"),closeModalBtn:document.querySelector("[data-modal-close-8]"),modal:document.querySelector("[data-modal-2]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.7ed2be87.js.map
---
+++ index.8ec0dbdf.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-5]"),closeModalBtn:document.querySelector("[data-modal-close-6]"),modal:document.querySelector("[data-modal-1]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.8ec0dbdf.js.map
